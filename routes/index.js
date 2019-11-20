ae151b66eb809b291696c5fff478461514dc9e90
Stephen Szemis <sszemis@stevens.edu>
2019-11-20 13:06:50 -0500

    Lots of routes. :)

---
+++ routes/index.js
@@ -0,0 +1,39 @@
+const loginRoute = require("./login");
+const logoutRoute = require("./logout");
+const homeRoute = require("./home");
+const wishRoute = require("./wish");
+const completedRoute = require("./completed");
+const detailsRoute = require("./details");
+const searchRoute = require("./search");
+
+let myLogger = function (req, res, next) {
+    const current_time = new Date().toUTCString();
+    const method = req.method;
+    const url = req.originalUrl;
+    let auth = false;
+    if (req.session.username) {
+        auth = true;
+    }
+    console.log("[" + current_time + "]: " + method + " " + url + " Auth: " + auth);
+    next();
+}
+
+const constructorMethod = app => {
+    // app.use("/", function (req, res, next) {
+    //     if (req.session.username) {
+    //         next();
+    //     } else {
+    //         res.status(403).sendFile(path.resolve("notLI.html"));
+    //     }
+    // });
+    app.use(myLogger);
+    app.use("/", homeRoute);
+    app.use("/login", loginRoute);
+    app.use("/logout", logoutRoute);
+    app.use("/wish", wishRoute);
+    app.use("/completed", completedRoute);
+    app.use("/details", detailsRoute);
+    app.use("/search", searchRoute);
+};
+
+module.exports = constructorMethod;
