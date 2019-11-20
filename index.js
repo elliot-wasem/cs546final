47f8df110eb213d4745910258f6a104c033cf8df
Stephen Szemis <sszemis@stevens.edu>
2019-11-20 12:34:29 -0500

    First index.js attempt

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const configRoutes = require("./routes");
+const exphbs = require('express-handlebars');
+
+// Cookie stuff
+// const session = require("express-session");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.engine('handlebars', exphbs({}));
+app.set('view engine', 'handlebars');
+configRoutes(app);
+
+app.listen(3000, () => {
+  console.log("We've now got a server!");
+  console.log("Your routes will be running on http://localhost:3000");
+});
